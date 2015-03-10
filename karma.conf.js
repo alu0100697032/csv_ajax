8d42a608dd3fa7e98bba1324ffa3300ac419f63a
alu0100837353 <alu0100837353@ull.edu.es>
2015-03-10 20:22:53 +0000

    Small change on karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
+    
+    client: {
+          mocha: {
+            ui: 'tdd'
+          }
+    },
 
 
     // list of files / patterns to load in the browser
