71ad1d3836203daf9e53a1e491b5797194a77748
Victor Hernandez Perez <alu0100697032.edu.es>
2015-03-15 18:23:09 +0000

    Trying AJAX

--- app.js
+++ app.js
@@ -25,13 +25,6 @@ app.get('/tests', function (req, res) {
 app.get('csv.js', function(req, res) {
     var isAjaxRequest = req.xhr;
     console.log(isAjaxRequest);
-      if (isAjaxRequest) {
-        console.log(req.query);
-        res.send('ajax request')
-      }
-      else {
-        res.send('not an ajax request');
-      }
 });
 
 app.listen(app.get('port'), function() {
