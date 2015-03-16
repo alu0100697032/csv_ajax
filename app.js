a68feb9adf4d984ca6f701d4d0e819cc1005e907
Victor Hernandez Perez <alu0100697032.edu.es>
2015-03-16 13:53:37 +0000

    Trying AJAX

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/csv', function (req, res) {
     console.log(isAjaxRequest);
     if (isAjaxRequest) {
         console.log(req.query);
-        res.send(new Buffer('csv'))
+        res.send(analizar())
     }
     else {
         res.send('not an ajax request');
